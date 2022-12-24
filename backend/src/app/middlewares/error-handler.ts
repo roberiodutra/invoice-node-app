2ccdce3e37de8ec49af6e7a3fc144bef30ab13b3
roberio <roberio-almeida@hotmail.com>
2022-12-24 03:18:35 -0300

    feat: config error handler middleware

---
+++ backend/src/app/middlewares/error-handler.ts
@@ -0,0 +1,30 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ZodError } from 'zod';
+import { ErrorCatalog, ErrorTypes } from '../helpers/ErrorCatalog';
+
+const ErrorHandler: ErrorRequestHandler = (
+  err: Error | ZodError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err instanceof ZodError) {
+    const response = err.issues.map((z) => z.message);
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: response });
+  }
+
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  const mappedError = ErrorCatalog[messageAsErrorType];
+
+  if (mappedError) {
+    const { code, message } = mappedError;
+    return res.status(code).json({ message });
+  }
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: 'Internal Error' });
+};
+
+export default ErrorHandler;
