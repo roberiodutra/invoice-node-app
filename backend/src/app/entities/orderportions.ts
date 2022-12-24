dc4d20d04c8803ce07c4faa779a99f8a1a819b4e
roberio <roberio-almeida@hotmail.com>
2022-12-24 09:30:20 -0300

    feat: create orderportions entity

---
+++ backend/src/app/entities/orderportions.ts
@@ -0,0 +1,37 @@
+export interface OrderPortionsProps {
+  nDup: string;
+  dVenc: string;
+  vDup: string;
+  availableToMarket: number;
+  createdAt: Date;
+  updatedAt: Date;
+  orderId?: number;
+}
+
+export class Buyer {
+  constructor(private props: OrderPortionsProps) {}
+
+  public get nDup() {
+    return this.props.nDup;
+  }
+
+  public get dVenc() {
+    return this.props.dVenc;
+  }
+
+  public get vDup() {
+    return this.props.vDup;
+  }
+
+  public get availableToMarket() {
+    return this.props.availableToMarket;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  public get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
