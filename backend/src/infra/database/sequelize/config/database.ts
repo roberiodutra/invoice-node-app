c1d282444d07a83d185ee7aa39aee6e094cd4148
roberio <roberio-almeida@hotmail.com>
2022-12-26 06:47:01 -0300

    build: config sequelize database

---
+++ backend/src/infra/database/sequelize/config/database.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const databaseConfig: Options = {
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  database: process.env.DB_NAME || 'cashforce_v3',
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'password',
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+};
+
+module.exports = databaseConfig;
