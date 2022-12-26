0e75e47756b7f807f342f8bbfa5a43055ce171a9
roberio <roberio-almeida@hotmail.com>
2022-12-26 09:29:39 -0300

    feat: create user controller and inject dependencies in user route

--- backend/src/routes/user-router.ts
+++ backend/src/routes/user-router.ts
@@ -1,7 +1,15 @@
+import { GetUser } from '@app/use-cases/users/get-user';
+import { SequelizeUserRepository } from '@infra/database/sequelize/repositories/sequelize-user-repository';
+import { UserController } from '@infra/http/controllers/user.controller';
 import { Router } from 'express';
 
 const route = Router();
 
-route.post('/sign_in');
+// Dependency injection
+const userRepository = new SequelizeUserRepository();
+const getUser = new GetUser(userRepository);
+const controller = new UserController(getUser);
+
+route.post('/sign_in', controller.findByEmail);
 
 export default route;
